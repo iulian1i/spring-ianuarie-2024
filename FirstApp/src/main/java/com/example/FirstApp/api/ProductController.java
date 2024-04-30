e43ed7a5eda4c2fdedad583f6b28c58477a59f3f
Iulian <35178081+geo600@users.noreply.github.com>
2024-04-30 20:26:22 +0300

    curs28

---
+++ FirstApp/src/main/java/com/example/FirstApp/api/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.FirstApp.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @GetMapping("/test")
+    public String productTest() {
+        return "<h1>test</h1>";
+    }
+
+    @GetMapping
+    public String productString() {
+        return "product String";
+    }
+
+}
